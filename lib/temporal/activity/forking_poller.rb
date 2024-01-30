b91789a9c625e1a8219ebbbc072724f1c051d1e4
Stu <stuart@getcensus.com>
2024-01-30 23:22:34 +0000

    basic implementation with a bunch of stuff commented out

---
+++ lib/temporal/activity/forking_poller.rb
@@ -0,0 +1,177 @@
+require 'temporal/activity/task_processor'
+require 'temporal/connection'
+require 'temporal/error_handler'
+require 'temporal/metric_keys'
+require 'temporal/middleware/chain'
+require 'temporal/scheduled_thread_pool'
+# require 'temporal/thread_pool'
+
+require 'grpc'
+
+module Temporal
+  class Activity
+    class ForkingPoller
+      DEFAULT_OPTIONS = {
+        thread_pool_size: 20,  # TODO: remove?
+        poll_retry_seconds: 0
+      }.freeze
+
+      def initialize(namespace, task_queue, activity_lookup, config, middleware = [], options = {})
+        @namespace = namespace
+        @task_queue = task_queue
+        @activity_lookup = activity_lookup
+        @config = config
+        @middleware = middleware
+        @shutting_down = false
+        @options = DEFAULT_OPTIONS.merge(options)
+      end
+
+      def start  # TODO: maybe just get rid of this
+        @shutting_down = false
+        # @thread = Thread.new(&method(:poll_loop))
+        poll_loop
+      end
+
+      def stop_polling  # TODO: how to do shutdown
+        @shutting_down = true
+        Temporal.logger.info('Shutting down activity poller', { namespace: namespace, task_queue: task_queue })
+      end
+
+      def cancel_pending_requests
+        connection.cancel_polling_request
+      end
+
+      # TODO: pretty sure this is not needed
+      # def wait
+      #   if !shutting_down?
+      #     raise "Activity poller waiting for shutdown completion without being in shutting_down state!"
+      #   end
+
+      #   thread.join
+      #   thread_pool.shutdown
+      #   heartbeat_thread_pool.shutdown
+      # end
+
+      private
+
+      attr_reader :namespace, :task_queue, :activity_lookup, :config, :middleware, :options, :thread
+
+      def connection
+        @connection ||= Temporal::Connection.generate(config.for_connection)
+      end
+
+      def shutting_down?
+        @shutting_down
+      end
+
+      def poll_loop
+        # Prevent the poller thread from silently dying
+        # Thread.current.abort_on_exception = true
+
+        last_poll_time = Time.now
+        metrics_tags = { namespace: namespace, task_queue: task_queue }.freeze
+
+        loop do
+          # thread_pool.wait_for_available_threads
+
+          return if shutting_down?  # TODO: not sure what should happen to this
+
+          time_diff_ms = ((Time.now - last_poll_time) * 1000).round
+          Temporal.metrics.timing(Temporal::MetricKeys::ACTIVITY_POLLER_TIME_SINCE_LAST_POLL, time_diff_ms, metrics_tags)
+          Temporal.logger.debug("Polling activity task queue", { namespace: namespace, task_queue: task_queue })
+
+          task = poll_for_task
+          last_poll_time = Time.now
+
+          Temporal.metrics.increment(
+            Temporal::MetricKeys::ACTIVITY_POLLER_POLL_COMPLETED,
+            metrics_tags.merge(received_task: (!task.nil?).to_s)
+          )
+
+          next unless task&.activity_type
+
+          # thread_pool.schedule { process(task) }
+          process(task)
+        end
+      end
+
+      def poll_for_task
+        connection.poll_activity_task_queue(namespace: namespace, task_queue: task_queue)
+      rescue ::GRPC::Cancelled
+        # We're shutting down and we've already reported that in the logs
+        nil
+      rescue StandardError => error
+        Temporal.logger.error("Unable to poll activity task queue", { namespace: namespace, task_queue: task_queue, error: error.inspect })
+
+        Temporal::ErrorHandler.handle(error, config)
+
+        sleep(poll_retry_seconds)
+
+        nil
+      end
+
+      def process(task)
+				Temporal.logger.info("forking to run activity")
+
+				GRPC.prefork
+
+				pid = fork do
+					GRPC.postfork_child
+
+					# TODO: needs to be closed
+					heartbeat_thread_pool_for_fork = 
+						ScheduledThreadPool.new(
+							1,
+							@config,
+							{
+								pool_name: 'heartbeat',
+								namespace: namespace,
+								task_queue: task_queue
+							}
+						)
+					process_inner(task, heartbeat_thread_pool_for_fork)
+				end
+
+				GRPC.postfork_parent
+
+				Temporal.logger.info("waiting on forked process (pid #{pid})")
+				Process.wait(pid)
+				Temporal.logger.info("forked process done!")
+      end
+
+      def process_inner(task, heartbeat_thread_pool)
+        middleware_chain = Middleware::Chain.new(middleware)
+
+        TaskProcessor.new(task, task_queue, namespace, activity_lookup, middleware_chain, config, heartbeat_thread_pool).process
+      end
+
+      def poll_retry_seconds
+        @options[:poll_retry_seconds]
+      end
+
+      # def thread_pool
+      #   @thread_pool ||= ThreadPool.new(
+      #     options[:thread_pool_size],
+      #     @config,
+      #     {
+      #       pool_name: 'activity_task_poller',
+      #       namespace: namespace,
+      #       task_queue: task_queue
+      #     }
+      #   )
+      # end
+
+      # def heartbeat_thread_pool
+      #   @heartbeat_thread_pool ||= ScheduledThreadPool.new(
+      #     options[:thread_pool_size],
+      #     @config,
+      #     {
+      #       pool_name: 'heartbeat',
+      #       namespace: namespace,
+      #       task_queue: task_queue
+      #     }
+      #   )
+      # end
+    end
+  end
+end
---
+++ lib/temporal/forking_activity_worker.rb
@@ -0,0 +1,195 @@
+require 'temporal/errors'
+require 'temporal/workflow/poller'
+require 'temporal/activity/forking_poller'
+require 'temporal/execution_options'
+require 'temporal/executable_lookup'
+require 'temporal/middleware/entry'
+
+module Temporal
+  class ForkingActivityWorker
+    # activity_thread_pool_size: number of threads that the poller can use to run activities.
+    #   can be set to 1 if you want no paralellism in your activities, at the cost of throughput.
+
+    # binary_checksum: The binary checksum identifies the version of workflow worker code. It is set on each completed or failed workflow
+    #   task. It is present in API responses that return workflow execution info, and is shown in temporal-web and tctl.
+    #   It is traditionally a checksum of the application binary. However, Temporal server treats this as an opaque
+    #   identifier and it does not have to be a "checksum". Typical values for a Ruby application might include the hash
+    #   of the latest git commit or a semantic version number.
+    #
+    #   It can be used to reset workflow history to before a "bad binary" was deployed. Bad checksum values can also
+    #   be marked at the namespace level. This will cause Temporal server to reject any polling for workflow tasks
+    #   from workers with these bad versions.
+    #
+    #   See https://docs.temporal.io/docs/tctl/how-to-use-tctl/#recovery-from-bad-deployment----auto-reset-workflow
+    def initialize(
+      config = Temporal.configuration,
+      # activity_thread_pool_size: Temporal::Activity::Poller::DEFAULT_OPTIONS[:thread_pool_size],  # TODO: remove?
+      # workflow_thread_pool_size: Temporal::Workflow::Poller::DEFAULT_OPTIONS[:thread_pool_size],
+      # binary_checksum: Temporal::Workflow::Poller::DEFAULT_OPTIONS[:binary_checksum],
+      activity_poll_retry_seconds: Temporal::Activity::ForkingPoller::DEFAULT_OPTIONS[:poll_retry_seconds]
+      # workflow_poll_retry_seconds: Temporal::Workflow::Poller::DEFAULT_OPTIONS[:poll_retry_seconds]
+    )
+      @config = config
+      # @workflows = Hash.new { |hash, key| hash[key] = ExecutableLookup.new }
+      @activities = Hash.new { |hash, key| hash[key] = ExecutableLookup.new }
+      @pollers = []
+      # @workflow_task_middleware = []
+      # @workflow_middleware = []
+      @activity_middleware = []
+      @shutting_down = false
+      @activity_poller_options = {
+        # thread_pool_size: activity_thread_pool_size,
+        poll_retry_seconds: activity_poll_retry_seconds
+      }
+      # @workflow_poller_options = {
+      #   thread_pool_size: workflow_thread_pool_size,
+      #   binary_checksum: binary_checksum,
+      #   poll_retry_seconds: workflow_poll_retry_seconds
+      # }
+      # @start_stop_mutex = Mutex.new  # TODO: pretty sure not needed
+    end
+
+    # def register_workflow(workflow_class, options = {})
+    #   namespace_and_task_queue, execution_options = executable_registration(workflow_class, options)
+
+    #   @workflows[namespace_and_task_queue].add(execution_options.name, workflow_class)
+    # end
+
+    # Register one special workflow that you want to intercept any unknown workflows,
+    # perhaps so you can delegate work to other classes, somewhat analogous to ruby's method_missing.
+    # Only one dynamic Workflow may be registered per task queue.
+    # Within Workflow#execute, you may retrieve the name of the unknown class via workflow.name.
+    # def register_dynamic_workflow(workflow_class, options = {})
+    #   namespace_and_task_queue, execution_options = executable_registration(workflow_class, options)
+
+    #   begin
+    #     @workflows[namespace_and_task_queue].add_dynamic(execution_options.name, workflow_class)
+    #   rescue Temporal::ExecutableLookup::SecondDynamicExecutableError => e
+    #     raise Temporal::SecondDynamicWorkflowError,
+    #           "Temporal::Worker#register_dynamic_workflow: cannot register #{execution_options.name} "\
+    #           "dynamically; #{e.previous_executable_name} was already registered dynamically for task queue "\
+    #           "'#{execution_options.task_queue}', and there can be only one."
+    #   end
+    # end
+
+    def register_activity(activity_class, options = {})
+      namespace_and_task_queue, execution_options = executable_registration(activity_class, options)
+      @activities[namespace_and_task_queue].add(execution_options.name, activity_class)
+    end
+
+    # Register one special activity that you want to intercept any unknown activities,
+    # perhaps so you can delegate work to other classes, somewhat analogous to ruby's method_missing.
+    # Only one dynamic Activity may be registered per task queue.
+    # Within Activity#execute, you may retrieve the name of the unknown class via activity.name.
+    def register_dynamic_activity(activity_class, options = {})
+      namespace_and_task_queue, execution_options = executable_registration(activity_class, options)
+      begin
+        @activities[namespace_and_task_queue].add_dynamic(execution_options.name, activity_class)
+      rescue Temporal::ExecutableLookup::SecondDynamicExecutableError => e
+        raise Temporal::SecondDynamicActivityError,
+              "Temporal::Worker#register_dynamic_activity: cannot register #{execution_options.name} "\
+              "dynamically; #{e.previous_executable_name} was already registered dynamically for task queue "\
+              "'#{execution_options.task_queue}', and there can be only one."
+      end
+    end
+
+    # def add_workflow_task_middleware(middleware_class, *args)
+    #   @workflow_task_middleware << Middleware::Entry.new(middleware_class, args)
+    # end
+
+    # def add_workflow_middleware(middleware_class, *args)
+    #   @workflow_middleware << Middleware::Entry.new(middleware_class, args)
+    # end
+
+    def add_activity_middleware(middleware_class, *args)
+      @activity_middleware << Middleware::Entry.new(middleware_class, args)
+    end
+
+    def start
+      # TODO: validate only one poller (probably at registration time)
+      # TODO: shutdown/trap signals?
+      activities.each_pair do |(namespace, task_queue), lookup|
+        pollers << activity_poller_for(namespace, task_queue, lookup)
+      end
+      pollers.first.start
+      stop
+    end
+
+    def start_old
+      @start_stop_mutex.synchronize do
+        return if shutting_down? # Handle the case where stop method grabbed the mutex first
+
+        trap_signals
+
+        workflows.each_pair do |(namespace, task_queue), lookup|
+          pollers << workflow_poller_for(namespace, task_queue, lookup)
+        end
+
+        activities.each_pair do |(namespace, task_queue), lookup|
+          pollers << activity_poller_for(namespace, task_queue, lookup)
+        end
+
+        pollers.each(&:start)
+      end
+      on_started_hook
+
+      # keep the main thread alive
+      sleep 1 until shutting_down?
+    end
+
+    def stop  # TODO
+    end
+
+    def stop_old
+      @shutting_down = true
+
+      Thread.new do
+        @start_stop_mutex.synchronize do
+          pollers.each(&:stop_polling)
+          while_stopping_hook
+          # allow workers to drain in-transit tasks.
+          # https://github.com/temporalio/temporal/issues/1058
+          sleep 1
+          pollers.each(&:cancel_pending_requests)
+          pollers.each(&:wait)
+        end
+        on_stopped_hook
+      end.join
+    end
+
+    private
+
+    attr_reader :config, :activity_poller_options, :workflow_poller_options,
+                :activities, :workflows, :pollers,
+                :workflow_task_middleware, :workflow_middleware, :activity_middleware
+
+    def shutting_down?
+      @shutting_down
+    end
+
+    def on_started_hook; end
+    def while_stopping_hook; end
+    def on_stopped_hook; end
+
+    # def workflow_poller_for(namespace, task_queue, lookup)
+    #   Workflow::Poller.new(namespace, task_queue, lookup.freeze, config, workflow_task_middleware, workflow_middleware,
+    #                        workflow_poller_options)
+    # end
+
+    def activity_poller_for(namespace, task_queue, lookup)
+      Activity::ForkingPoller.new(namespace, task_queue, lookup.freeze, config, activity_middleware, activity_poller_options)
+    end
+
+    def executable_registration(executable_class, options)
+      execution_options = ExecutionOptions.new(executable_class, options, config.default_execution_options)
+      key = [execution_options.namespace, execution_options.task_queue]
+      [key, execution_options]
+    end
+
+    def trap_signals
+      # %w[TERM INT].each do |signal|
+      #   Signal.trap(signal) { stop }
+      # end
+    end
+  end
+end
